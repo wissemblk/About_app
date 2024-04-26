2340d563a4bd7db5f038697248f3849214e26216
wissBLK04 <wissembelkhodja@gmail.com>
2024-04-26 13:56:52 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import "./App.css";
+import React from "react"; 
+import "./Components/Styling.css";
 import Navbar from "./Components/NavBar.jsx";
 import Rectangle from "./Components/Rectangle.jsx";
 
